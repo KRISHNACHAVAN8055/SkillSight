1059e0e152bd8a7785d926d3d59ff2416f81e88e
krishna chavan <krishnachavan@krishnas-MacBook-Air.local>
2026-04-11 02:32:08 +0530

    add render deployment

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ app.register_blueprint(bp)
 if __name__ == '__main__':
     from startup import check_and_seed
     check_and_seed()
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=False)
